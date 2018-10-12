871118c9cc712f904a61232c27f641432a3388de
NemesisMate <nemesismatedev@gmail.com>
2018-10-12 19:44:49 +0200

    Fixed small typo

--- src/test/java/com/github/nemesismate/gigtrial/logic/config/ConfigFromConsoleTest.java
+++ src/test/java/com/github/nemesismate/gigtrial/logic/config/ConfigFromConsoleTest.java
@@ -50,7 +50,7 @@ public class ConfigFromConsoleTest {
         whenGather();
 
         thenTheGameConfigContainsGivenMatchMode();
-        thenTheGameConfigContainsGivenOuputMode();
+        thenTheGameConfigContainsGivenOutputMode();
     }
 
     @Test
